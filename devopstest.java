e94a7c66d86713f6a4782475071149b3b8cd5d72
SanaeAdli <65692170+SanaeAdli@users.noreply.github.com>
2021-03-21 22:38:30 +0100

    Revert "Edit devopstest.java"

--- devopstest.java
+++ devopstest.java
@@ -2,7 +2,7 @@ package devopsTP;
 
 public class devopstest {
 	public static void main(String[] args) {
-    System.out.println("Hello World! We are Imane and Sanae!!"+ " .............nous modifions le fichier le fichier"); 
+    System.out.println("Hello World! We are Imane and Sanae!!"); 
 }
 
 }
